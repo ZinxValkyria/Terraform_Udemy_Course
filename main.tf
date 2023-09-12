675c75a7b3739639ada1f55a03f570f2afa12c70
Blain Noone <nooneblain@gmail.com>
2023-09-12 12:11:32 +0100

    added key-pair

--- main.tf
+++ main.tf
@@ -67,3 +67,24 @@ resource "aws_security_group" "mtc_sg" {
   }
 
 }
+resource "aws_key_pair" "mtc_auth" {
+  key_name   = "mtckey"
+  public_key = file("~/.ssh/mtckey.pub")
+
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.my_ami.id
+
+  tags = {
+    Name = "dev_node"
+  }
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mySUBNET.id
+
+  root_block_device {
+    volume_size = 10
+  }
+}
